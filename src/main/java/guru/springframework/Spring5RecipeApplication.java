bd0d84c985c9f1d9299e15d94acd024b562a29da
pawelklodzinski <pawelklodzinski@onet.pl>
2021-06-27 10:48:02 +0200

    package rename.

--- src/main/java/guru/springframework/Spring5RecipeApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5RecipeAppApplication {
+public class Spring5RecipeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+		SpringApplication.run(Spring5RecipeApplication.class, args);
 	}
 }
